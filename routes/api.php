334cf537744096dfd10a03e160a11402cc1207da
Hasan Basri <hasannolur06@gmail.com>
2025-03-03 04:56:08 +0300

    users api denemesi

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,5 @@ Route::controller(LoginController::class)
     ->group(function () {
         Route::post('login', 'store');
     });
+
+Route::get('/users', fn() => User::all());
